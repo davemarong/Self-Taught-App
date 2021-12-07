f82ca2ef9ce08bb52ae2959453ee8244273a0d75
davemarong <kmarong@hotmail.com>
2021-12-07 11:20:50 +0100

    Refactor code before merge

--- src/components/mainSubjects/modal/DeleteSubjectModal.js
+++ src/components/mainSubjects/modal/DeleteSubjectModal.js
@@ -1,10 +1,5 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import CardContent from "@material-ui/core/CardContent";
-import Container from "@material-ui/core/Container";
+
 import { useDispatch, useSelector } from "react-redux";
 import {
   change_main_subjects,
@@ -12,8 +7,16 @@ import {
   get_top_level_index,
   change_subject_type,
 } from "../../../redux/actions/index";
+
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import CardContent from "@material-ui/core/CardContent";
+import Container from "@material-ui/core/Container";
 import CloseIcon from "@material-ui/icons/Close";
 import IconButton from "@material-ui/core/IconButton";
+
 import usePushDataToServer from "../../customHooks/usePushDataToServer";
 
 export default function DeleteSubjectModal({
@@ -29,19 +32,11 @@ export default function DeleteSubjectModal({
   const { updateMainSubjectsInServer, updateSecondarySubjectsInServer } =
     usePushDataToServer();
 
-  const deleteMainSubject = (subjectIndex) => {
-    const updatedMainSubjects = mainSubjects.filter(
-      (subject, id) => id != subjectIndex
-    );
-    return updatedMainSubjects;
+  const deleteSubject = (subjectIndex, subject) => {
+    const updatedSubjects = subject.filter((subject, id) => id != subjectIndex);
+    return updatedSubjects;
   };
-  const deleteSecondarySubject = (subjectIndex) => {
-    const updatedSecondarySubjects = secondarySubjects.filter(
-      (subject, id) => id != subjectIndex
-    );
-    return updatedSecondarySubjects;
-  };
-  console.log(deleteSubjectModal);
+
   return (
     <div>
       <Container style={{ marginTop: 200, maxWidth: 400 }}>
@@ -67,14 +62,20 @@ export default function DeleteSubjectModal({
                   <Button
                     onClick={() => {
                       if (deleteSubjectModal.subject === "mainsubject") {
-                        const updatedSubject = deleteMainSubject(index);
+                        const updatedSubject = deleteSubject(
+                          index,
+                          mainSubjects
+                        );
                         updateMainSubjectsInServer(updatedSubject);
                         dispatch(change_main_subjects(updatedSubject));
                         closeDeleteSubjectModal();
                       } else if (
                         deleteSubjectModal.subject === "secondarysubject"
                       ) {
-                        const updatedSubject = deleteSecondarySubject(index);
+                        const updatedSubject = deleteSubject(
+                          index,
+                          secondarySubjects
+                        );
                         updateSecondarySubjectsInServer(updatedSubject);
                         dispatch(change_secondary_subjects(updatedSubject));
                         closeDeleteSubjectModal();
